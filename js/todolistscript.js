fc572a58331db2a0bba65376f6da0e9717ad551d
Ben Hansen <hansen.ben.e@gmail.com>
2019-10-14 10:16:36 -0600

    commit 3 started initialization functions/classes

--- js/todolistscript.js
+++ js/todolistscript.js
@@ -1,24 +1,31 @@
 // make list class
 class ListObject {
-    constructor (taskAmount) {
-        //taskAmount is the amount of tasks in the list, for organization with multiple lists
-        this.taskAmount = taskAmount;
-    }
-
-
-    initList () {
-
+    constructor (myname, tasks) {
+        this.tasks = tasks; //should be an array
+        this.myname = myname;
     }
 }
 
-class TaskObject extends ListObject {
-    constructor (taskAmount, myNum) {
+class TaskObject {
+    constructor (list, myNum) {
         //myNum is the number(order) of the task within its own list
-        super(taskAmount);
+        this.list = list;
         this.myNum = myNum;
     }
 
-    createTask (imp) { //an added imp(important) variable to pin/prioritize a task if it returns true
+}
 
-    }
+let lists = [];
+let curList = "";
+
+
+function createList (name) { //this will be called after a function that prompts the user to name the list
+    let newList = new ListObject(name);
+    let newTask = new TaskObject(newList.name, 0);
+}
+
+function nameList() {
+    //prompt window for a name
+    let newName = "";
+    createList(newName);
 }
