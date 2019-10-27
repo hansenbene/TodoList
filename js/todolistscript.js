07a94a861088e51a1133abf18fb6dbc815340b02
Ben Hansen <hansen.ben.e@gmail.com>
2019-10-26 19:36:49 -0600

    almost finished, adding ability to edit and then styling the page

--- js/todolistscript.js
+++ js/todolistscript.js
@@ -1,30 +1,39 @@
 // make list class
 class ListObject {
     constructor (myname, tasks, index) {
-        this.tasks = tasks; //should be an array
+        this.tasks = tasks; //should be an array containing (task) TaskObjects
         this.myname = myname;
         this.index = index;
+        // this.Initialtask = new TaskObject(index, 0); //list comes with an empty task, maybe with a placeholder text
+
     }
+
+    // AddNewTask() {
+    //     let taskname = "hello there";
+    //     let newTask = new TaskObject(this.index, this.tasks.length, taskname);
+    //     // this.tasks.push(newTask);
+    //     updateTasks();
+    // }
 }
 
 class TaskObject {
-    constructor (list, myNum) {
+    constructor (listid, myNum, myname) {
         //myNum is the number(order) of the task within its own list
-        this.list = list;
+        //list will be a number representing the index of the list in the lists array
+
+        //I plan to have the add task button located by the list name, and the edit task button by each individual task
+
+        this.listid = listid;
         this.myNum = myNum;
+        this.myname = myname;
         this.complete = false;
+        this.template = `<li class="task" onclick="editTask();">${this.myname}</li>`;
     }
 
 }
 
 let lists = [];
 let curList;
-
-
-// function createList (name) { //this will be called after a function that prompts the user to name the list
-//     let newList = new ListObject(name);
-//     let newTask = new TaskObject(newList.name, 0);
-// }
 
 function addListKeyUp(event) {
     switch(event.which) {
@@ -34,18 +43,78 @@ function addListKeyUp(event) {
     }
 }
 
+function addTaskKeyUp(event) {
+    switch(event.which) {
+        case 13:
+            createTask();
+            break;
+    }
+}
+
 function addList() {
-    //prompt window for a name
     let name = $("#listInput").val();
     console.log(name);
-    if(name !== "") {
+    let checkName = /\S/;
+    if(checkName.test(name)) {
         let list = new ListObject(name, [], lists.length);
         lists.push(list);
         $("#listInput").val("");
+        selectList(list.index);
+        displayTasks();
         updateLists();
     }
-    // let newName = "";
-    // createList(newName);
+}
+
+
+function createTask() { //create a task manually since I can't get functions within classes to work
+    let taskname = $("#taskInput").val();
+    let checkName = /\S/;
+    if(checkName.test(taskname)) {
+        let newTask = new TaskObject(curList.index, curList.tasks.length, taskname);
+        curList.tasks.push(newTask);
+        $("#taskInput").val("");
+        updateTasks();
+    }
+
+}
+
+function displayTasks() { //so that we don't see the input for tasks if there is no list selected
+
+    let taskbox = document.getElementById("taskBox");
+    let toolbox = document.getElementById("taskOptions");
+
+    if(curList) {
+        taskbox.style.visibility = "visible";
+        toolbox.style.visibility = "visible";
+    }
+    else {
+        taskbox.style.visibility = "hidden";
+        toolbox.style.visibility = "hidden";
+    }
+}
+
+// function addTask(taskname) { //function will just initiate the function inside the list object
+//     // let name; //the list item will hold an empty text field upon initialization for the user to input the task
+//     curList.AddNewTask(); //call the selected list to create a new task
+//
+// } //this hasn't been working because of an error that says the function AddNewTask() doesn't exist
+
+function editTask() {
+    console.log(this);
+}
+
+function removeList (index) {
+    if(lists[index] == curList) {
+        curList = null;
+        $("#listTitle").html("");
+        displayTasks();
+    }
+    lists.splice(index, 1);
+    for(let i = 0; i < lists.length - index; i++) { //reassign indexes for the lists to match lists.length
+        lists[index + i].index -= 1;
+    }
+
+    updateLists();
 }
 
 function updateLists() {
@@ -53,15 +122,39 @@ function updateLists() {
     let listItems = $("#listNames");
     listItems.empty();
     for(let i = 0; i < lists.length; i++) {
-        let list = `<li>${lists[i].myname}</li>`;
+        let list = `<li><span class="listlist" onclick="selectList(${i})">${lists[i].myname}</span><div class="controls"><button onclick="removeList(${i})"><i class="fas fa-trash"></i></button><button onclick="editList()"><i class="fas fa-edit"></button></div></li>`;
         listItems.append(list);
     }
     saveData();
 }
 
+function updateTasks() {
+    console.log(curList.tasks);
+    let listItems = $("#listItems");
+    listItems.empty();
+    for(let i = 0; i < curList.tasks.length; i++) {
+        let task = `<li class="task">${curList.tasks[i].myname}</li>`;
+        listItems.append(task);
+    }
+    saveData();
+}
+
+function selectList(listid) { // select list to show in the list display on the right
+    let listtitle = $("#listTitle");
+    // console.log(listid);
+    listtitle.html(`${lists[listid].myname}`);
+    curList = lists[listid];
+    displayTasks();
+    updateTasks();
+}
+
+
+
 function saveData() {
     localStorage.setItem("listData", JSON.stringify(lists));
 }
+
+
 
 function getData() {
     let temp = localStorage.getItem("listData");
